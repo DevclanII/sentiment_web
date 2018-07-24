5a898f1f82833582b31ca84911d6b2624a3df637
BigUncleYemi <olayemioyebanji911@gmail.com>
2018-07-24 14:18:50 +0100

    starting project

---
+++ src/component/LandingPage/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class Header extends React.Component {
+    render() {
+      return (
+       <div className="container-fluid">
+         <div className="row">
+           <div className="col-sm-6" />
+           <div className="col-sm-6">
+             <div className="col-sm-6">
+               
+             </div>
+             <div className="col-sm-6">
+               
+             </div>
+           </div>
+         </div>
+       </div>
+       );
+   }
+
+}
+
+export default Header;
