4f31bcf066e1374396495277baf5d1e453180749
BigUncleYemi <olayemioyebanji911@gmail.com>
2018-07-24 23:00:51 +0100

    done with header and responsiveness

--- src/component/LandingPage/Header.js
+++ src/component/LandingPage/Header.js
@@ -1,18 +1,48 @@
 import React from 'react';
 
+const Nav = () => (
+<ul className = "nav">
+    <li className="nav-item">
+      <a className="nav-link active" href="#">Home</a>
+    </li>
+    <li className="nav-item">
+      <a className="nav-link" href="#">About</a>
+    </li>
+    <li className="nav-item">
+      <a className="nav-link" href="#">Docs</a>
+    </li>
+    <li className="nav-item">
+      <a className="nav-link " href="#">Try It Live</a>
+    </li>
+    <li className="nav-item">
+      <a className="nav-link " href="#">Contact Us</a>
+    </li>
+  </ul>
+);
+
+const Faq = ( ) => (
+  <div></div>
+);
+
 class Header extends React.Component {
     render() {
       return (
        <div className="container-fluid">
-         <div className="row">
-           <div className="col-sm-6" />
-           <div className="col-sm-6">
-             <div className="col-sm-6">
-               
+          <div className="row">
+            <div className="backImg col-lg-6" style={{ height: '100vh' }}>
+           </div>
+            <div className="col-lg-6" style={{ height: '100vh' }}>
+             <div className="row" id="head">
+               <Nav />
+               <div className="pos-abs">
+                  <h2 className="display-4 text" style={{width: '70%', fontSize: '2.5rem', marginBottom: '30px'}}>
+                    Quisque in velit id ante suscipit convallis in ac mauris. Aliquam
+                    eget venenatis urna, quis facilisis libero. 
+                  </h2>
+                  <button type="button" class="btn btn-outline-dark round" style={{ borderRadius: '30px'}}>Read More</button>
+               </div>
              </div>
-             <div className="col-sm-6">
-               
-             </div>
+              
            </div>
          </div>
        </div>
