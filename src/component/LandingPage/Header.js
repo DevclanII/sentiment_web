00f1d82b359771e6e00aa3fdbb2f0fa3f70b53b1
BigUncleYemi <olayemioyebanji911@gmail.com>
2018-11-01 11:44:08 +0100

    Fix: tush up the landing page

--- src/component/LandingPage/Header.js
+++ src/component/LandingPage/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Nav from './Nav';
+import {Link} from 'react-router-dom';
 
 class Header extends React.Component {
   render() {
@@ -7,20 +8,17 @@ class Header extends React.Component {
       <div className="container-fluid">
         <div className="row">
           <div className="cover col-lg-6" style={{position: 'absolute'}}/>
-          <div className="backImg col-lg-6" style={{ height: '94vh' }}>
-          </div>
+          <div className="backImg col-lg-6" style={{ height: '94vh' }}/>
           <div className="col-lg-6" style={{ height: '94vh' }}>
             <div className="row" id="head">
               <Nav />
               <div className="p-4 my-auto">
-                <h4 className="text-lg-left text-md-center" style={{width: '70%', marginBottom: '30px'}}>
-                  Want To Elavate Your Brand ?<br />
-                  What That Clean Sheet Online Presences ?<br />
-                  Know What Your Audiences Think and Feel About You and Your Campaings<br />
-                  And Give Them The Right Content At The Right Time<br />
-                  And <strong>We Are Here To Help To Help You</strong>
-                </h4>
-                <button type="button" className="btn btn-outline-dark round" href="/Try" style={{ borderRadius: '30px'}}>Try It Now</button>
+                <h5 className="text-lg-left tt text-md-center" style={{width: '61%', margin: '45px 0px', fontSize: '30px'}}>
+                Want to Know What Your Audiences Think About You and Your Campaings<br />
+                <br />
+                <strong>We Are Here To Help You</strong>
+                </h5>
+                <Link to="/Try"><button type="button" className="btn btn-outline-dark round" style={{ borderRadius: '30px'}}>Try It Now</button></Link>
               </div>
             </div>
           </div>
